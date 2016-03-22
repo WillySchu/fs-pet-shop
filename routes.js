2059fe69cacfef2778870e284c66bf5ba30a8cf9
Will Schumacher <meintodengel@gmail.com>
2016-03-22 14:11:11 -0700

    linting

--- routes.js
+++ routes.js
@@ -1,61 +1,61 @@
-var fs = require('fs');
-var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
-var routes = {
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+const routes = {
   '/pets': function(req, res) {
     if (req.method === 'GET') {
-      fs.readFile(petsPath, 'utf8', function(err, data) {
-        if(err) throw err;
+      fs.readFile(petsPath, 'utf8', (err, data) => {
+        if (err) throw err;
 
         res.statusMessage = 200;
         res.end(data);
-      })
+      });
     } else if (req.method === 'POST') {
-      fs.readFile(petsPath, 'utf8', function(readErr, data) {
-        if(readErr) throw readErr;
+      fs.readFile(petsPath, 'utf8', (readErr, data) => {
+        if (readErr) throw readErr;
 
-        var animals = JSON.parse(data);
-        req.on('data', function(chunk) {
-          var animal = JSON.parse(chunk.toString());
+        const animals = JSON.parse(data);
+        req.on('data', (chunk) => {
+          const animal = JSON.parse(chunk.toString());
           animal.age = parseInt(animal.age, 10);
           if (animal.name && animal.kind && typeof animal.age === 'number') {
             animals.push(animal);
-            fs.writeFile(petsPath, JSON.stringify(animals), function(writeErr) {
-              if(writeErr) throw writeErr;
-            })
+            fs.writeFile(petsPath, JSON.stringify(animals), (writeErr) => {
+              if (writeErr) throw writeErr;
+            });
             res.end(chunk);
           }
           res.statusMessage = 400;
-          res.end('Bad Request')
-        })
-      })
+          res.end('Bad Request');
+        });
+      });
     }
   },
   '/pets/': function(req, res) {
     if (req.method === 'GET') {
-      fs.readFile(petsPath, 'utf8', function(err, data) {
-        if(err) throw err;
+      fs.readFile(petsPath, 'utf8', (err, data) => {
+        if (err) throw err;
 
-        var index = req.url[req.url.length - 1];
-        if(index !== '/') {
+        const index = req.url[req.url.length - 1];
+        if (index !== '/') {
           var animals = JSON.parse(data);
           if (index > animals.length - 1) {
             res.statusMessage = 404;
-            res.end('No entry at index ' + index)
+            res.end('No entry at index ${index}');
           }
-          var animal = animals[index];
+          const animal = animals[index];
           res.statusMessage = 200;
           res.end(JSON.stringify(animal));
         } else {
           routes['/pets'](req, res);
         }
-      })
+      });
     }
   },
   unknown: function(req, res) {
     res.statusMessage = 404;
     res.end('Not found');
-  }
-}
+  },
+};
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-var routes = require('./routes');
-var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
-var pets = /^\/pets\/(.*)$/;
-var port = process.env.PORT || 8080;
-var server = http.createServer(function(req, res) {
+const http = require('http');
+const routes = require('./routes');
+const pets = /^\/pets\/(.*)$/;
+const port = process.env.PORT || 8080;
+const server = http.createServer((req, res) => {
   if (routes[req.url]) {
     routes[req.url](req, res);
   } else if (req.url.match(pets)) {
@@ -16,5 +13,5 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(port, function() {
-  console.log('listening on port ' + port);
+  console.log('listening on port ${port}');
 });
